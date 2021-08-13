e77ad8de93487b00bb7d6519e0ba3c8188c9a3f5
chenxingyu0830 <chenxingyu0830@gmail.com>
2021-08-13 08:04:02 +0800

    Feat：update resume

--- js/main.js
+++ js/main.js
@@ -1,74 +1,85 @@
 let resume = `# 陈星宇
 188-6808-5296 | chenxingyu0830@gmail.com | 杭州
 GitHub: https://github.com/chenxingyu0830
-Website: https://smallyu.top/resume/
 Blog: https://smallyu.top/ 
-19届毕业生 | 前端工程师 
+2年前端开发经验 | 本科
 
 
 ## 专业技能
+- React
+  - 熟悉 React 及有相关库的开发经验，理解生命周期、组件通信、React Hooks、虚拟Dom 等相关概念。
+  - 使用或了解 Redux、Mobx、DvaJS、SWR 等状态管理库，React Router 路由库。
 - Vue
-  - 有 Vue 个人开发项目经验，阅读过官方文档，熟悉 Vue 常用功能，如Vue的各个生命周期、组件之间如何通信、双向绑定等。了解并使用过 Vue-Cli , Vue-Router , Vuex。
+  - 有 Vue 个人开发项目经验，掌握组件及组件间数据传递的使⽤、响应式原理、路由等。
+- TypeScript
+  - 有 TypeScript 工作开发经验，有一定自己理解。理解 接⼝、常⽤类型、可选链、枚举、泛型 等相关知识。
 - Webpack
   - 掌握使用 Webpack 打包项目的经验，了解其管理资源的方法，如 css-loader 、 babel-loader 等。
-- jQuery
-  - 了解jQuery常用API，利用jQuery实现原生JS的操作内容，进行页面的交互设计、效果实现等。
-- HTML5 , CSS3 & JavaScript
-  - 能狗独立制作精美网页，掌握 CSS3 动画、过渡效果、响应式等常用技术。
-- 移动端页面
+- PC / 移动端 页面
+  - 熟练使用 Ant Design / Ant Design Mobile UI库，有 Sass / Less 开发经验
+  - 掌握 Flex / Grid 等布局方式，可以合理使用语义化H5标签及 善于使用 BEM 命名规范 或 驼峰法。
   - 会使用 REM、vw/vh、FastClick 等技术制作适配手机设备的页面。
-- 前端框架
-  - 理解 MVC、MVVM 等思想，可以熟练使用Vue。
+  - 能狗独立制作精美网页，掌握 CSS3 动画、过渡效果、响应式等常用技术。
+- 可视化
+  - 了解并使用echarts，AntV G2/BizCharts 等相关可视化库。
+- CI/CD & 单元测试
+  - 了解 CI/CD 流程及内容，了解常用 CI 工具如 Jenkins，CircleCI。
+  - 了解单元测试内容，个人项目使用过 Jest 库，能通过 CI 工具 test 项目相关代码，保证代码可维护性。
+- Git
+  - 熟悉 git flow 工作流模式，具有代码仓库协同⼯作的能⼒，Github / Gitlab 使用经验。
 - 其他
-  - 了解Node.js（使用过基于Node.js的框架Express），能简单实现路由功能。
-  - 了解HTTP协议，了解常见的 Web 性能优化方案。
+  - 了解Node.js（使用过 Express/Koa, 做过 hollo world demo），能简单实现路由功能。
   - 具有通过Google解决问题的能力，有自己的技术博客。
 
 ## 工作经历
-### 浙江浙大网新图灵信息科技有限公司
-2018/10 - 2019/02
-- 职位：前端实习生
-- 主要技术：\`HTML\`、\`CSS\`、\`jQuery\`、\`Bootstrap\`、\`JSONP\`
-- 工作内容：主要负责公司内部后台网站的维护工作，并且通过jQuery和Bootstrap来实现添加新功能模块的添加修改和模块样式的修改。在这几个月的实习期间，收获了对jQuery的了解与使用。同时也实现了后台模块的几个小功能。
+### 杭州连帆科技有限公司
+2019/06 - 至今
+- 职位：前端工程师
+1. **"智护+移动护理" PC/PDA**（2020/09 - 至今）
+  - 协同开发基于 \`React\`技术栈的 \`移动护理解决方案\`平台，采用\`前后端分离\`架构。
+  - PC端及移动端 采用\`React + React Router + Redux + Ant Design / mobile \`的解决方案，网络请求二次封装了feach。
+  - PC端难点在于 例如"体征批量录入"界面，医院护士希望能有 word文档 的使用体验，所以需要对 Antd 的 Table组件进行二次封装，包括支持类似回车键自动切换到下一体征项的功能需求。
+  - 移动端难点在于 Antd mobile 的组件功能不是特别完善，需要自己二次封装类似 Modal 组件，实现 router 跳转的功能。
+  - 同时在项目代码层面底层封装了许多基础类库，能够满足工作当中常用的场景，减少将近于30%的代码重复工作。
+  - 为提升开发效率，两端均使用\`GitLab CI/CD\`进行持续集成和持续部署，实现了构建部署自动化。
+2. **"分诊导医" 叫号 + 屏相关页面 & 后台管理系统**（2020/06 - 至今）
+  - 独立开发基于 \`React\`技术栈的 \`分诊叫号 + 各类医院屏\`平台，采用\`前后端分离\`架构。
+  - PC端及移动端 采用\`React + React Router + Mobx + Ant Design \`的解决方案，网络请求二次封装了feach。
+  - 项目难点在于 数据的同步性，因为属于叫号系统，需要实时从后台获取最新数据，页面数据相对简单。为保证页面的性能采用 Mobx 状态管理，并且大量使用\`工厂模式\`，\`单例模式\`等设计模式。
+  - 项目亮点：取号屏/报到屏 点击模拟键盘输入，打印取号/报到标签；诊区屏/诊间屏/流水屏 自定义配置满足医院个性化需求 & 采用\`vw/vh\` \`rem\`等方式自适应横竖屏。
+  - 因为系统页面及功能打包体积较小，经过公司内部讨论采用 \`Webpack 多页面打包\`的实践方式，保证两个路由下的单页面开发，方便代码维护。
+  - 历时一年多时间，系统成功上线十几家医院，获得医院口碑及医生护士满意度。
+  - 为提升开发效率，使用\`GitLab CI/CD\`进行持续集成和持续部署，实现了构建部署自动化。
+3. **"护理管理信息系统" PC端**（2019/06 - 2020/06）
+  - 协同开发基于 \`React\`技术栈的 \`移动护理解决方案\`平台，采用\`前后端分离\`架构。
+  - 负责排班模块，人员管理，考试培训 等模块的开发工作。
+  - 项目难点在于 服务几百家医院，项目体量较大。许多功能模块开发时需要考虑多种场景，需要不断沟通及需求变更。并且属于2014年的项目，当时采用\`ExtJS\`的项目开发，导致后续页面重构工作难度较大，同时还要自学 ExtJS 及 善于总结和网上查找相关知识点。
+  - 为提升开发效率，使用\`GitLab CI/CD\`进行持续集成和持续部署，实现了构建部署自动化。
 
 ## 项目经验
-### “Let's share”多人共享博客平台
-![“Let's share”多人共享博客平台](./images/blog-project.jpg)
-- 关键词： \`Vue.js\`、\`Vue-cli\`、\`Vue Router\`、\`Vuex\`、\`Scss\`、\`ES6\`、\`Axios\`、\`Webpack\`
+### “S1mple react” React-UI库
+![“S1mple react” React-UI库](./images/blog-project.jpg)
+- 关键词： \`UI 组件库\`、\`React Hooks\`、\`React Router\`、\`TypeScript\`、\`npm\`、\`Webpack\`、\`Jest\`、\`CircleCi\`
+- 源码链接：https://github.com/chenxingyu0830/vue-blog-client
+- 预览链接：https://smallyu.top/vue-blog-client/dist/.
+- 描述：个人独立开发的 React UI 组件库，使用 React Hooks + TypeScript 开发，并已发布到 npm 中。由此掌握了 webpack 打包、 npm 发布流程、 CircleCi 自动化持续集成等。
+
+### “share blod”多人共享博客平台
+![“share blod”多人共享博客平台](./images/blog-project.jpg)
+- 关键词： \`Vue.js\`、\`Vue-cli\`、\`Vue Router\`、\`Vuex\`、\`Grid布局\`、\`Scss\`、\`ES6\`、\`Axios\`、\`Webpack\`
 - 源码链接：https://github.com/chenxingyu0830/vue-blog-client
 - 预览链接：https://smallyu.top/vue-blog-client/dist/.
 - 描述：这个项目是基于Vue全家桶实现的多人共享博客平台。功能有用户登录/注册，博客的创建/编辑/删除等功能。\`通过Vue Router实现路由的跳转、异步加载、权限验证等，通过vuex实现状态管理，用Axios获取数据\`，并对接口进行了封装。
 
-### 仿“CNode社区”网站
-![仿“CNode社区”网站](./images/cnode-project.jpg)
-- 关键词： \`Vue.js\`、\`Vue-cli\`、\`Vue Router\`、\`Scss\`、\`ES6\`、\`Axios\`、\`Webpack\`
-- 源码链接: https://github.com/chenxingyu0830/cnode
-- 预览链接: https://smallyu.top/cnode/dist/
-- 描述: 这个项目是基于Vue-cli脚手架工具，仿照CNode社区实现的。CNode社区官网提供了后端数据的接口。功能包括查看\`首页的博客，点击查看博客详情，用户信息，实现简单的分页组件\`。
-
-### 仿华为音乐项目
-![仿华为音乐项目](./images/music-project.jpg)
-- 关键词： \`原生JS\`、\`CSS3\`、\`Scss\`、\`ES6\`、\`手势库\`
-- 源码链接：https://github.com/chenxingyu0830/huawei-music
-- 预览链接: https://smallyu.top/huawei-music/dist/
-- 描述：这个项目是仿照华为音乐制作的项目，包括\`暂停播放、上一曲、下一曲、滑屏显示歌词\`等功能。其中滑屏显示歌曲是通过封装的手势库里的函数实现的。
-
-### 给你一只皮卡丘
-![给你一只皮卡丘](./images/pikachu-project.jpg)
-- 关键词： \`纯CSS实现卡通形象\`、\`Promise\`、\`变速\`、\`Markdown\`、\`响应式布局\`、\`适配移动端\`
-- 源码链接：https://github.com/chenxingyu0830/make-a-pikachu
-- 预览链接：https://smallyu.top/make-a-pikachu/
-- 描述：这个项目是通过\`原生JS\`实现的，以动画的效果展示如果从无到有通过\`CSS实现\`皮卡丘的卡通形象。另外还添加了变速/结束动画功能。
+### 在线简历生成器
+![在线简历生成器](./images/resume-project.jpg)
+- 关键词： \`jQuery\`、\`CSS3\`、\`Flex布局\`、\`变速\`、\`跳过动画\`、\`Promise\`
+- 源码链接：https://github.com/chenxingyu0830/animation-resume
+- 预览链接：https://smallyu.top/animation-resume/
+- 描述：这个项目动态的展示了一个 Markdown 简历的制作过程，通过Flex布局实现页面布局，jQuery实现简历模块化。另外还添加了 \`变速/跳过动画\` 功能。
 
 
 ## 教育经历
-### 衢州学院
-2014/09 - 2017/06
-- 计算机应用技术 大专 电气与信息工程学院
-- GPA：3.4 / 4.0（专业前10%）
-- 荣誉/奖项：优秀团员（2015-2016）、“互联网+”大学生创新创业大赛金奖（2016）
-- 二等奖学金（2015-2016）、三等奖学金（2016-2017）、优秀毕业生（2017）
-
 ### 浙江大学城市学院
 2017/09 - 2019/06
 - 电子信息工程 本科 信息与电气工程学院
@@ -272,19 +283,19 @@ a.downloadResume{
 `
 
 var codeInputTimeoutID, resumeInputTimeoutID
-writeCode(code_ready, '').then(()=>{
-  $('#skip_btn').children().show()
-  $('#skip_btn').click(()=>{ showFinalResult() })
-  writeResume(resume).then(()=>{
+
+
+writeCode(code_ready, '').then(() => {
+  writeResume(resume).then(() => {
     structuredResume()
-    writeCode(code_marked, code_ready).then(()=>{
-        writeCode(code_better_resume, code_ready + code_marked).then(()=>{
-          addAvatar()
-          $('#speed_btn').hide()
-          $('#skip_btn').hide()
-          $('#code_body').removeClass('breathe')
-          $('#paper').addClass('breathe')
-        })
+    writeCode(code_marked, code_ready).then(() => {
+      writeCode(code_better_resume, code_ready + code_marked).then(() => {
+        addAvatar()
+        $('#speed_btn').hide()
+        $('#skip_btn').hide()
+        $('#code_body').removeClass('breathe')
+        $('#paper').addClass('breathe')
+      })
     })
   })
 })
@@ -323,39 +334,42 @@ function writeCode(code, origin) {
   return new Promise((resolve) => {
     $('#code_body').addClass('breathe')
     $('#paper').removeClass('breathe')
-    codeInputTimeoutID  = setTimeout(write, duration)
+    codeInputTimeoutID = setTimeout(write, duration)
 
-    function write(){
+    $('#skip_btn').children().show()
+    $('#skip_btn').click(() => {showFinalResult()})
+
+    function write() {
       n += 1
-      code_body.innerHTML = Prism.highlight(origin + code.substring(0,n), Prism.languages.css, 'css')
+      code_body.innerHTML = Prism.highlight(origin + code.substring(0, n), Prism.languages.css, 'css')
       code_style.innerHTML = origin + code.substring(0, n)
       code_body.scrollTop = code_body.scrollHeight
       if (n === code.length) {
         resolve.call(undefined)
       } else {
-        codeInputTimeoutID = setTimeout(write, duration)        
+        codeInputTimeoutID = setTimeout(write, duration)
       }
     }
   })
 }
 
 
-function writeResume(resume){
+function writeResume(resume) {
   let n = 0
-  return new Promise((resolve)=>{
+  return new Promise((resolve) => {
     $('#code_body').removeClass('breathe')
     $('#paper').addClass('breathe')
-    resumeInputTimeoutID = setTimeout(write,duration)
+    resumeInputTimeoutID = setTimeout(write, duration)
 
     function write() {
       n += 1
-      paper.innerHTML = resume.substring(0,n)
+      paper.innerHTML = resume.substring(0, n)
       paper.scrollTop = paper.scrollHeight
-      if(n === resume.length){
+      if (n === resume.length) {
         paper.scrollTop = 0
         return resolve(undefined)
-      }else{
-        resumeInputTimeoutID = setTimeout(write,duration)
+      } else {
+        resumeInputTimeoutID = setTimeout(write, duration)
       }
     }
 
@@ -363,28 +377,27 @@ function writeResume(resume){
 }
 
 
-function addAvatar(){
-  return new Promise((resolve)=>{
+function addAvatar() {
+  return new Promise((resolve) => {
     $('#paper > #information').append($('img.avatar'))
     writeCode(code_photo, code_ready + code_marked + code_better_resume)
   })
 }
 
 
-function structuredResume(){
+function structuredResume() {
   $('#paper')[0].innerHTML = marked(resume)
-   $('#paper').prepend('<div id="information"></div>', '<div id="skills"></div>', '<div id="jobs"></div>', '<div id="project"></div>', '<div id="education"></div>')
+  $('#paper').prepend('<div id="information"></div>', '<div id="skills"></div>', '<div id="jobs"></div>', '<div id="project"></div>', '<div id="education"></div>')
   $('#information').append($('h1'), $('p').first())
   $('#skills').append($('h2:contains("专业技能")'), $('ul').first())
   $('#education').append($('h2:contains("教育经历")'))
-  $('<div class="school"></div>').append($('h3:contains("衢州学院")') ,$('#paper > p').eq(-2), $('#paper > ul').eq(-2)).appendTo($('#education'))
   $('<div class="school"></div>').append($('h3:contains("浙江大学城市学院")'), $('#paper > p').last(), $('#paper > ul').last()).appendTo($('#education'))
   $('#jobs').append($('h2:contains("工作经历")'))
-  $('h3:contains("公司")').each((index,element) => {
-    $('<div class="experience"></div>').append($(element), $('#paper > p').first(), $('#paper > ul').first()).appendTo($('#jobs'))
+  $('h3:contains("公司")').each((index, element) => {
+    $('<div class="experience"></div>').append($(element), $('#paper > p').first(), $('#paper > ul').first(), $('#paper > ol').first()).appendTo($('#jobs'))
   })
   $('#project').append($('h2:contains("项目经历")'), $('#paper').children().not($('div')))
-  $('#project h3').each((index,element) => {
+  $('#project h3').each((index, element) => {
     $('<div class="demo"></div>').append($(element), $('#project > p').first(), $('#project > ul').first()).appendTo($('#project'))
   })
 }
